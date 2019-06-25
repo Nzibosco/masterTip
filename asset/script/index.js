44a0cdbf66fcc591d8a0827a3b58b5533d83bf35
Bosco <bosco@MacBook-Air.local>
2019-06-24 21:21:57 -0500

    added function to calculate tip

--- asset/script/index.js
+++ asset/script/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
 
-    //define tip
+    //define global variables
     let tip;
+    let share;
 
     // var to grab user inputs 
     let bill = $("#bill").val();
@@ -26,16 +27,47 @@ $(document).ready(function () {
         console.log("tip: " + tip + " bill: " + bill + " tip %: " + tipPercent + " people on table: " + peopleOnTable);
 
         //assign values to the Dashboard 
-        $("#amount").text(tip);
+        
         $("#bill-final").text(bill);
         $("#tip-final").text(tipPercent);
         $("#table-final").text(peopleOnTable);
 
+        //calculate tip
+        // ...first check if the tip will be splitted then split the tip among table members
+        if ($("#radio-yes").checked = true) {
+            console.log("checked");
 
+            function tipToPay() {
+                tip = bill * tipPercent / 100;
+                console.log(tip);
+                // now share the tip among table members
+                share = tip / peopleOnTable;
+                console.log(share);
 
+                // assign tip and share values to tthe page
+                $("#amount").text(tip);
+                $("#share-final").text(share);
+                
+            };
+            tipToPay();
+        } else {
+            // if no split needed, we will go straight to calculating tip only
+            function tipToPay() {
+                tip = bill * tipPercent / 100;
+                console.log(tip);
+                share = tip;
+
+                // assign tip and share values to tthe page
+                $("#amount").text(tip);
+                $("#share-final").text(share);
+
+                // set people on the table to one
+                $("#table-final").text(1);
+                
+            };
+            tipToPay();
+        }
     })
-
-    //calculate tip
 
 
 })
