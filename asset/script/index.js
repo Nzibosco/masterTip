194f59f30536c41bf9af76e4196ffddfd127f868
Bosco <bosco@MacBook-Air.hsd1.tn.comcast.net>
2019-06-25 22:28:32 -0500

    added form protection and final bill amount calculation

--- asset/script/index.js
+++ asset/script/index.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     //define global variables
     let tip;
     let share;
+    let finalBill;
 
     // var to grab user inputs 
     let bill = $("#bill").val();
@@ -28,25 +29,28 @@ $(document).ready(function () {
 
         //assign values to the Dashboard 
         
-        $("#bill-final").text(bill);
         $("#tip-final").text(tipPercent);
         $("#table-final").text(peopleOnTable);
 
         //calculate tip
         // ...first check if the tip will be splitted then split the tip among table members
-        if ($("#radio-yes").checked = true) {
+
+        if ($("#radio-yes").is(":checked")) {
             console.log("checked");
 
             function tipToPay() {
                 tip = bill * tipPercent / 100;
                 console.log(tip);
+
                 // now share the tip among table members
                 share = tip / peopleOnTable;
                 console.log(share);
+                finalBill = parseFloat(bill) + parseFloat(tip);
 
                 // assign tip and share values to tthe page
                 $("#amount").text(tip);
                 $("#share-final").text(share);
+                $("#bill-final").text(finalBill);
                 
             };
             tipToPay();
@@ -55,11 +59,12 @@ $(document).ready(function () {
             function tipToPay() {
                 tip = bill * tipPercent / 100;
                 console.log(tip);
-                share = tip;
+                finalBill = parseFloat(bill) + parseFloat(tip);
 
-                // assign tip and share values to tthe page
+                // assign tip, final bill and share values to the page
                 $("#amount").text(tip);
-                $("#share-final").text(share);
+                $("#share-final").text(tip);
+                $("#bill-final").text(finalBill);
 
                 // set people on the table to one
                 $("#table-final").text(1);
